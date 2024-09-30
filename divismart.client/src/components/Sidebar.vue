7e2785058280f95f452f8114d469ac5468110a18
Matheus <matheustrilhakoch@hotmail.com>
2024-09-29 22:21:30 -0300

    Iniciando com sidebar

---
+++ divismart.client/src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="sidebar">
+        <a href="#">Menu</a>
+        <a href="#">Carteiras</a>
+        <a href="#">Dividendos</a>
+        <a href="#">Relatórios</a>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    
+</style>
