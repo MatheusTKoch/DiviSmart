25c3c10f84a6ab49641748134efa43c3cd7567b7
Matheus <matheustrilhakoch@hotmail.com>
2024-10-31 21:44:28 -0300

    Adicionando titulo e botao header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,36 @@
 </script>
 
 <template>
- <div class="header"></div>
+ <div class="header">
+    <div class="titulo">DiviSmart</div>
+    <button class="login">Login</button>
+ </div>
 </template>
 
 <style scoped>
+button.login {
+    background-color: transparent;
+    position: relative;
+    left: 170vh;
+    top: -2vh;
+    color: ghostwhite;
+    border-color: ghostwhite;
+}
+
+div.titulo {
+    color: ghostwhite;
+    position: relative;
+    left: 10vh;
+    top: 2vh;
+    font-weight: 800;
+    font-size: x-large;
+    margin: 0;
+    padding: 0;
+}
+
 div.header {
     width: 100%;
     min-height: 5vh;
+    display: inline;
 }
 </style>
