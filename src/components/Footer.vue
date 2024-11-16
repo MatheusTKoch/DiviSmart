de9be680e7f7c5356812ba254cb73ce305be3564
Matheus <matheustrilhakoch@hotmail.com>
2024-11-15 21:15:50 -0300

    Ajustando footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,15 +3,36 @@
 </script>
 
 <template>
-    <div class="footer"></div>
+    <div class="footer">
+        <div class="contact">Contato</div>
+        <img src="../assets/github.png" alt="github" class="git">
+        <div class="marca">DivSmart</div>
+    </div>
 </template>
 
 <style scoped>
 div.footer {
-    position: fixed;
+    position: relative;
     bottom: 0;
-    width: 100%;
+    width: 100vh;
     min-height: 5vh;
 }
 
+div.contact {
+    color: whitesmoke;
+    position: relative;
+    left: 20%;
+}
+
+/* div.git {
+    position: relative;
+    height: 10vh;
+    left: 20vh;
+} */
+
+div.marca {
+    position: relative;
+    left: 80%;
+    color: whitesmoke;
+}
 </style>
