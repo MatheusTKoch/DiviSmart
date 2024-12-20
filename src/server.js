e2e73a35ff06d45195619b8d0275cc8a765144a7
Matheus <matheustrilhakoch@hotmail.com>
2024-12-20 19:59:22 -0300

    Removido arquivo db, adicionado no server Ajustado import de variaveis env

--- src/server.js
+++ src/server.js
@@ -1,6 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-import db from './server/index.js';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({path: path.resolve(__dirname, '../.env')});
+
+const db = mysql.createConnection({
+    database: process.env.VITE_DATABASE_DB, 
+    user: process.env.VITE_USER_DB, 
+    password: process.env.VITE_PASSWORD_DB, 
+    host: process.env.VITE_HOST_DB
+  });
 
 const app = express();
 
@@ -12,7 +26,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     db.connect((err) => {console.log(err)});
-    res.json({mess:"Teste com sucesso"});
+    res.json({teste: 'teste'});
 });
 
 app.post("/users", (req, res) => {
@@ -24,7 +38,7 @@ app.post("/users", (req, res) => {
     res.json({teste:"teste"});
 })
 
-const PORT = 8080;
+const PORT = process.env.VITE_PORT;
 
 app.listen(PORT, () => {
     console.log(`Server rodando na porta ${PORT}.`);
