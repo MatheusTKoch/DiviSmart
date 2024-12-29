ec536638277057930d5f13a44d041630b1211190
Matheus <matheustrilhakoch@hotmail.com>
2024-12-29 18:43:48 -0300

    Adicionando sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="sidebar">
+    <ul class="lista">
+        <li>Carteira</li>
+        <li>Dividendos</li>
+        <li>Relatórios</li>
+    </ul>
+</div>
+</template>
+
+<style scoped>
+div.sidebar {
+    position: absolute;
+    top: 20%;
+    width: 14vw;
+    border-right: 2px solid black;
+    height: 100vh;
+    z-index: 1;
+}
+
+ul.lista {
+    position: relative;
+    min-width: 10vw;
+    text-align: left;
+    padding-top: 2vh;
+    bottom: 10%;
+}
+
+
+li {
+    list-style-type: none;
+    color: ghostwhite;
+    font-size:calc(100% + 0.25vw);
+}
+</style>
