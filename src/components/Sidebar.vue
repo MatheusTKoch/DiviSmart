39fc5c7960965bc35d65178ac015850983a39965
Matheus <matheustrilhakoch@hotmail.com>
2024-12-31 17:14:58 -0300

    Ajustando bordas e selecao de menu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,45 @@
 <script setup lang="ts">
+let menu = false;
+let carteira = false;
+let dividendos = false;
+let relatorios = false;
 
+function underlineSelect(element: String) {
+    if (element == 'menu') {
+        menu = true;
+        carteira = false;
+        dividendos = false;
+        relatorios = false;
+        element = '';
+    } else if (element == 'carteira') {
+        menu = false;
+        carteira = true;
+        dividendos = false;
+        relatorios = false;
+        element = '';
+    } else if (element == 'dividendos') {
+        menu = false;
+        carteira = false;
+        dividendos = true;
+        relatorios = false;
+        element = '';
+    } else if (element == 'relatorios') {
+        menu = false;
+        carteira = false;
+        dividendos = false;
+        relatorios = true;
+        element = '';
+    }
+}
 </script>
 
 <template>
 <div class="sidebar">
     <ul class="lista">
-        <li><RouterLink to="/menu/carteira">Carteira</RouterLink></li>
-        <li><RouterLink to="/menu/dividendos">Dividendos</RouterLink></li>
-        <li><RouterLink to="/menu/relatorios">Relatórios</RouterLink></li>
+        <li><RouterLink @click="underlineSelect('menu')" :style="menu ? 'text-decoration: underline' : 'text-decoration: none'" to="/menu">Menu</RouterLink></li>
+        <li><RouterLink @click="underlineSelect('carteira')" :style="carteira ? 'text-decoration: underline' : 'text-decoration: none'" to="/menu/carteira">Carteira</RouterLink></li>
+        <li><RouterLink @click="underlineSelect('dividendos')" :style="dividendos ? 'text-decoration: underline' : 'text-decoration: none'" to="/menu/dividendos">Dividendos</RouterLink></li>
+        <li><RouterLink @click="underlineSelect('relatorios')" :style="relatorios ? 'text-decoration: underline' : 'text-decoration: none'" to="/menu/relatorios">Relatórios</RouterLink></li>
     </ul>
 </div>
 </template>
@@ -30,6 +62,7 @@ div.sidebar {
 }
 
 ul.lista {
+    margin-top: 0;
     min-width: 10vw;
     text-align: left;
     padding-top: 2vh;
