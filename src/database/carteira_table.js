16d30c1bf8b6ff77446828a7ac9d94d904e428d5
Matheus <matheustrilhakoch@hotmail.com>
2025-01-04 23:18:14 -0300

    Criando tabelas carteira e ativo Acoes

---
+++ src/database/carteira_table.js
@@ -0,0 +1,25 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({path: path.resolve(__dirname, '../../.env')});
+
+const db = mysql.createConnection({
+    database: process.env.VITE_DATABASE_DB, 
+    user: process.env.VITE_USER_DB, 
+    password: process.env.VITE_PASSWORD_DB, 
+    host: process.env.VITE_HOST_DB
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    let sql = 'CREATE TABLE IF NOT EXISTS carteiras (CarteiraID int NOT NULL AUTO_INCREMENT, Nome varchar(100) NOT NULL, PRIMARY KEY(CarteiraID))'
+    db.query(sql, (err) => {
+        if (err) throw err;
+        console.log("Tabela criada!");
+        db.end();
+    })
+});
