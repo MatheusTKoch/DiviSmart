d221b4a716a645da3093581f2f97cc5034a06be3
Matheus <matheustrilhakoch@hotmail.com>
2025-01-04 23:34:51 -0300

    Adicionada table ativos_fii

---
+++ src/database/ativos_fii_table.js
@@ -0,0 +1,25 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({path: path.resolve(__dirname, '../../.env')});
+
+const db = mysql.createConnection({
+    database: process.env.VITE_DATABASE_DB, 
+    user: process.env.VITE_USER_DB, 
+    password: process.env.VITE_PASSWORD_DB, 
+    host: process.env.VITE_HOST_DB
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    let sql = 'CREATE TABLE IF NOT EXISTS ativos_fii (AtivoFiiID int NOT NULL AUTO_INCREMENT, Quantidade INT NOT NULL, ValorInvestido float(15,2), DataCadastro DATETIME NOT NULL, fiiID INT NOT NULL, FOREIGN KEY (fiiID) REFERENCES fundo_imobiliario(FundoImobiliarioID), PRIMARY KEY(AtivoFiiID))'
+    db.query(sql, (err) => {
+        if (err) throw err;
+        console.log("Tabela criada!");
+        db.end();
+    })
+});
