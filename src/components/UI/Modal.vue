4b001c8ddb7bb0d284f21395d27a52dc674b378f
Matheus <matheustrilhakoch@hotmail.com>
2025-01-08 21:08:33 -0300

    Removendo render de rotas Ajustando modal

--- src/components/UI/Modal.vue
+++ src/components/UI/Modal.vue
@@ -4,24 +4,40 @@
 
 <template>
     <div class="conteudo">
-        <div class="titulo">Cadastro de Carteiras</div>
-        <label for="nomeCarteira">Nome da Carteira</label>
+        <div class="titulo">Cadastro de Carteiras<a class="fechar" @click="$emit('mostrarModal')">X</a></div>
+        <label for="nomeCarteira">Nome da Carteira: </label>
         <input type="text" id="nomeCarteira">
         <div class="botoes">
-            <button>Cadastrar</button>
-            <button @click="$emit('mostrarModal')">Voltar</button>
+            <button class="cadastrar">Cadastrar</button>
+            <button @click="$emit('mostrarModal')" class="voltar">Voltar</button>
         </div>
     </div>
 </template>
 
 <style scoped>
 div.conteudo {
+    padding: 30%;
     position: relative;
     text-align: center;
+    white-space: nowrap;
+    border: 1px solid black;
+    border-radius: 20px;
+    background-image: linear-gradient(180deg, rgba(17,43,148,1) 2%, rgba(0,239,255,1) 100%);
+}   
+
+a.fechar {
+    left: 20%;
+    position: relative;
+    color: ghostwhite;
+    cursor: pointer;
+}
+
+div.botoes {
+    padding: 5%;
 }
 
 div.titulo {
-    font-size: large;
+    font-size: x-large;
 }
 
 label {
--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,6 @@ import HomeView from './components/Home.vue';
 import NotFoundView from './components/Utilitarios/NotFound.vue';
 import RegisterView from './components/Forms/Register.vue';
 import LoginView from './components/Forms/Login.vue';
-import MenuView from './components/Home.vue';
 import CarteiraView from './components/Utilitarios/Carteira.vue';
 import DividendosView from './components/Utilitarios/Dividendos.vue';
 import RelatoriosView from './components/Utilitarios/Relatorios.vue';
@@ -13,7 +12,7 @@ const routes = [
     {path: '/', name:'home', component: HomeView},
     {path: '/register', name: 'register', component: RegisterView},
     {path: '/login', name: 'login', component: LoginView},
-    {path: '/menu', name: 'menu', component: MenuView, children:[
+    {path: '/menu', name: 'menu', children:[
         {path: '/menu/carteira', component: CarteiraView},
         {path: '/menu/dividendos', component: DividendosView},
         {path: '/menu/relatorios', component: RelatoriosView},]},
