842bc9b07ff24e2cc96d629f40f086303ab0ed29
Matheus <matheustrilhakoch@hotmail.com>
2025-01-16 22:11:41 -0300

    Adicionada validacao para localStorage vazio Adicionada funcao de login no menu e componentes

--- src/components/Utilitarios/Menu.vue
+++ src/components/Utilitarios/Menu.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
 import Header from '../UI/Header.vue';
 import Sidebar from '../UI/Sidebar.vue';
-import router from '../../router';
+import axios from 'axios';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+onMounted(() => {
+    verifyUser();
+})
+
+function verifyUser() {
+    axios.post('http://localhost:8080/session', {
+        usID: localStorage.getItem('usID'),
+        sID: localStorage.getItem('sID'),
+        exp: localStorage.getItem('exp')
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+        if(err.response.data == 'Sessao expirada' && err.response.status == 401) {
+            localStorage.removeItem('usID');
+            localStorage.removeItem('exp');
+            localStorage.removeItem('sID');
+            router.push('/login');
+        }
+
+        if(err.response.data == 'Sem dados na localStorage' && err.response.status == 401) {
+            router.push('/login');
+        }
+    });
+}
 </script>
 
 <template>
