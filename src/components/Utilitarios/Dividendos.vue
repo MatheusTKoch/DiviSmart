94d64a2c2daa51da760d1b3f6746bece96601274
Matheus <matheustrilhakoch@hotmail.com>
2025-01-25 16:41:09 -0300

    Ajustando UI para Dividendos

--- src/components/Utilitarios/Dividendos.vue
+++ src/components/Utilitarios/Dividendos.vue
@@ -37,8 +37,32 @@ function verifyUser() {
 <template>
     <Header showPerfil></Header>
     <Sidebar></Sidebar>
+    <div class="conteudo">
+        <div class="titulo_div">Dividendos</div>
+        <div class="descricao">Visualize os relatórios de acordo com o periodo desejado:</div>
+        <label for="carteira">Carteira:</label>
+        <select name="carteira" required></select>
+        <label for="data_inicial">Data Inicial:</label>
+        <input type="date" required>
+        <label for="data_final">Data Final:</label>
+        <input type="date" required>
+    </div>  
 </template>
 
 <style scoped>
+div.conteudo {
+    position: absolute;
+    top: 20%;
+    left: 40%;
+    color: ghostwhite;
+}
 
+div.titulo_div {
+    font-size: xx-large;
+    text-align: center;
+}
+
+div.descricao {
+    font-size: large;
+}
 </style>
