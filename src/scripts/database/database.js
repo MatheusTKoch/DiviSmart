95edbc9f8058e1ec127a9a00aed4b72fef2f6b96
MatheusTKoch <matheust_koch@hotmail.com>
2025-12-06 10:29:52 -0300

    Replace MySQL with PostgreSQL for database connection and update dependencies

--- src/scripts/database/database.js
+++ src/scripts/database/database.js
@@ -1,25 +1,31 @@
-import mysql from 'mysql2';
+import pg from 'pg';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+const { Pool } = pg; 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({path: path.resolve(__dirname, '../../../.env')});
 
-const db = mysql.createConnection({
-    database: process.env.VITE_DATABASE_DB, 
+const db = new Pool({
     user: process.env.VITE_USER_DB, 
     password: process.env.VITE_PASSWORD_DB, 
     host: process.env.VITE_HOST_DB
 });
 
-db.connect((err) => {
-    if (err) throw err;
-    let sql = 'CREATE DATABASE IF NOT EXISTS divismart'
-    db.query(sql, (err) => {
-        if (err) throw err;
-        console.log("Tabela criada!");
+db.connect().then(() => {
+        console.log('Conectado com sucesso');
+        const sql = `
+            CREATE DATABASE divismart
+        `;
+        return db.query(sql);
+    })
+    .then(() => {
+        console.log('Banco Criado!');
         db.end();
     })
-});
+    .catch(err => {
+        console.error('Erro no comando:', err);
+        db.end();
+    });
