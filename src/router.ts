40ad1dc271477e206e52977611ebe03865aae100
MatheusTKoch <matheust_koch@hotmail.com>
2026-01-11 00:05:01 -0300

    refactor: standardize route definitions and improve code formatting in router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,28 +1,37 @@
 import { createWebHistory, createRouter } from "vue-router";
 
-import HomeView from './components/Home.vue';
-import NotFoundView from './components/Utilitarios/NotFound.vue';
-import RegisterView from './components/Forms/Register.vue';
-import LoginView from './components/Forms/Login.vue';
-import CarteiraView from './components/Utilitarios/Carteira.vue';
-import DividendosView from './components/Utilitarios/Dividendos.vue';
-import RelatoriosView from './components/Utilitarios/Relatorios.vue';
+import HomeView from "./components/Home.vue";
+import NotFoundView from "./components/Utilitarios/NotFound.vue";
+import RegisterView from "./components/Forms/Register.vue";
+import LoginView from "./components/Forms/Login.vue";
+import MenuView from "./components/Utilitarios/Menu.vue"; 
+import CarteiraView from "./components/Utilitarios/Carteira.vue";
+import DividendosView from "./components/Utilitarios/Dividendos.vue";
+import RelatoriosView from "./components/Utilitarios/Relatorios.vue";
 
 const routes = [
-    {path: '/', name:'home', component: HomeView},
-    {path: '/register', name: 'register', component: RegisterView},
-    {path: '/login', name: 'login', component: LoginView},
-    {path: '/menu', name: 'menu', children:[
-        {path: '/menu/carteira', component: CarteiraView},
-        {path: '/menu/dividendos', component: DividendosView},
-        {path: '/menu/relatorios', component: RelatoriosView},]},
-    {path: '/not-found', name:'notFound', component: NotFoundView},
-    {path: '/:catchAll(.*)*', redirect: '/not-found'}
+  { path: "/", name: "home", component: HomeView },
+  { path: "/register", name: "register", component: RegisterView },
+  { path: "/login", name: "login", component: LoginView },
+
+  {
+    path: "/menu",
+    name: "menu",
+    component: MenuView,
+    children: [
+      { path: "carteira", name: "carteira", component: CarteiraView },
+      { path: "dividendos", name: "dividendos", component: DividendosView },
+      { path: "relatorios", name: "relatorios", component: RelatoriosView },
+    ],
+  },
+
+  { path: "/not-found", name: "notFound", component: NotFoundView },
+  { path: "/:catchAll(.*)*", redirect: "/not-found" },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
