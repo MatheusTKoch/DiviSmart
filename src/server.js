d8c3e695febe3fb0660f2408fe56ca65520f04ff
MatheusTKoch <matheust_koch@hotmail.com>
2026-08-16 21:39:21 -0300

    fix: update CORS configuration to use includes for origin validation

--- src/server.js
+++ src/server.js
@@ -60,13 +60,15 @@ const allowedOrigins = [
 
 app.use(cors({ 
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Bloqueado pelo CORS'));
     }
   },
-  credentials: true 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'] 
 }));
 
 //Express
